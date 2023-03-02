ba551f00b68104d0a666e96dae1b2b3e67b0be2b
Pedro <pedropaivadev@gmail.com>
2023-03-01 21:08:12 -0300

    class03 - min30:00 - drawFingers

--- pages/titles/src/services/handGestureService.js
+++ pages/titles/src/services/handGestureService.js
@@ -1,17 +1,17 @@
-import { gestureStrings, knownGestures } from "../util/gestures.js"
-
 export default class HandGestureService {
   #gestureEstimator
   // #fingerpose
   #handPoseDetection
   #handsVersion 
   #detector = null
+  #gestureStrings
 
-  constructor({ fingerpose, handPoseDetection, handsVersion }) {
+  constructor({ fingerpose, handPoseDetection, handsVersion, gestureStrings, knownGestures }) {
     this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
     // this.#fingerpose = fingerpose
     this.#handPoseDetection = handPoseDetection
     this.#handsVersion = handsVersion
+    this.#gestureStrings = gestureStrings
   }
 
   async estimate(keypoints3D) {
@@ -37,7 +37,7 @@ export default class HandGestureService {
       const {x, y} = hand.keypoints.find(keypoint => keypoint.name === 'index_finger_tip')
       yield { event: result.name, x, y }
 
-      console.log('detected', gestureStrings[result.name])
+      console.log('detected', this.#gestureStrings[result.name])
     }
   }
 
